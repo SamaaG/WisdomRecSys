6829be10d306c8829286268ca73f2780c48458f3
SamaaGazzaz <sora.jp@hotmail.Com>
2016-07-04 13:02:36 -0500

    First step of including W2V

---
+++ InformationExtraction/src/main/scala/pipeline/Word2Vec.scala
@@ -0,0 +1,13 @@
+package pipeline
+
+import org.apache.spark.SparkContext
+
+/**
+  * Created by Samaa on 7/4/2016.
+  */
+object Word2Vec {
+  def word2vec(filePath: String,sc: SparkContext){
+    
+  }
+
+}
